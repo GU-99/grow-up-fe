433c275f4633ac3780eb025e5891e555b28397ad
Seok93 <gkdltjr@naver.com>
2024-05-17 16:52:17 +0900

    Config: #3 TS config & Vite config에 절대 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +10,21 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     css: true,
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/constants') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@layouts', replacement: path.resolve(__dirname, 'src/layouts') },
+      { find: '@mocks', replacement: path.resolve(__dirname, 'src/mocks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@stories', replacement: path.resolve(__dirname, 'src/stories') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
   },
 });
