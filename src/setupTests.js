98fdd7ec209426da211871f5a50af857a584e61a
Seok93 <gkdltjr@naver.com>
2024-05-17 14:32:42 +0900

    Config: #3 라이브러리 추가 및 라이브러리 기본  환경 설정

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+
+import { beforeAll, afterEach, afterAll } from 'vitest';
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
