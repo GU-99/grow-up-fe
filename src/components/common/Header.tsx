3b703ace39e0af9aad1bab8dfb3fe79dc8a0b422
Seok93 <gkdltjr@naver.com>
2024-06-19 23:17:22 +0900

    UI: #16 공용 Header CSS 설정 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,6 +5,7 @@ import { FiHome } from 'react-icons/fi';
 
 export default function Header() {
   // ToDo: 로그인 기능 구현 후, 로그아웃 로직 연결하기
+  // ToDo: 유저 정보를 토대로 응원 아이디 변경하기
   return (
     <header className="flex h-header items-center justify-between bg-main px-15">
       <div>
@@ -13,7 +14,7 @@ export default function Header() {
         </Link>
       </div>
       <nav className="flex items-center">
-        <div className="text-white">User 님의 Grow Up! 응원합니다.</div>
+        <div className="tracking-tight text-white">User 님의 Grow Up! 응원합니다.</div>
         <NavLink to="/" className="ml-10 hover:brightness-90">
           {({ isActive }) => <FiHome className={`size-20 ${isActive ? 'text-selected' : 'text-white'}`} />}
         </NavLink>
