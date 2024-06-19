073469f665953cedb1f26e34c92d5ddd4cd14909
kimpearl3599 <kimpearl3599@gmail.com>
2024-06-19 18:10:24 +0900

    Feat: #3 TeamSettingPage 기능구현

--- src/pages/setting/TeamSettingPage.tsx
+++ src/pages/setting/TeamSettingPage.tsx
@@ -18,29 +18,53 @@ const user = {
   ],
 };
 
-function TeamList(props) {
-  for (let i = 0; i < user.team.length; ) {
-    if (user.team[i].status === props) {
-      return <div>{user.team[i].name}</div>;
-    }
+// mode가 Y이면, 탈퇴하기 버튼 노출하고 클릭 시 status N으로 변경
+// mode가 N이면, 가입하기, 거부하기 버튼 노출
+// 가입하기 클릭 시 status Y로 변경 / 거부하기 클릭 시 user.team에서 삭제
+function ButtonList(props) {
+  if (props === 'Y') {
+    return (
+      <div>
+        <button type="button">탈퇴하기</button>
+      </div>
+    );
+  }
+  if (props === 'N') {
+    return (
+      <div>
+        <button type="button">가입하기</button>
+        <button type="button">거부하기</button>
+      </div>
+    );
   }
 }
-// mode가 Y이면 가입현황 nav 선택되고, status가 Y인 데이터 노출
-// mode가 N이면 대기현황 nav 선택되고, status가 N인 데이터 노출
+
+function TeamList(props) {
+  const list = [];
+  for (let i = 0; i < user.team.length; i) {
+    if (user.team[i].status === props.status) {
+      list.push(<div>{user.team[i].name}</div>);
+    }
+  }
+  return list;
+}
+
+// mode가 Y이면, status가 Y인 데이터 노출
+// mode가 N이면, status가 N인 데이터 노출
 function TeamSettingPage() {
   const [mode, setMode] = useState('Y');
-  const list = TeamList(mode);
   return (
     <>
       <nav>
-        <button type="button" onChange={() => setMode('Y')}>
+        <button type="button" value="Y" onClick={(event) => setMode(event.currentTarget.value)}>
           가입현황
         </button>
-        <button type="button" onChange={() => setMode('N')}>
+        <button type="button" value="N" onClick={(event) => setMode(event.currentTarget.value)}>
           대기현황
         </button>
       </nav>
-      {list}
+      <TeamList status={mode} />
+      <ButtonList status={mode} />
     </>
   );
 }
