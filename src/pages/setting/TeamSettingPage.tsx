6c1d9f87cee6cbcab9ea63508073821e96e5b0e6
kimpearl3599 <kimpearl3599@gmail.com>
2024-06-20 18:09:55 +0900

    Feat: #4 TeamSettingPage 기능구현_2

--- src/pages/setting/TeamSettingPage.tsx
+++ src/pages/setting/TeamSettingPage.tsx
@@ -1,49 +1,95 @@
+/* eslint-disable react/destructuring-assignment */
 import { useState } from 'react';
 
 const user = {
   id: 1,
-  name: 'momoco',
+  name: 'mococo',
   password: '1234',
-  email: 'momoco@com',
+  email: 'mococo@com',
   phone: '010-1234-7777',
   img: '',
-  comment: "Hi, I'm Momoco!",
-  link1: 'momoco@github.com',
+  comment: "Hi, I'm Mococo!",
+  link1: 'mococo@github.com',
   link2: null,
   link3: null,
   team: [
-    { id: 1, name: 'GU-99', status: 'Y' },
-    { id: 2, name: '김찌와 쏘주', status: 'Y' },
-    { id: 3, name: '모코코', status: 'N' },
+    { id: 1, name: 'GU-99', status: 'Y', admin: 1 },
+    { id: 2, name: '김찌와 쏘주', status: 'Y', admin: 3 },
+    { id: 3, name: '모코코', status: 'N', admin: 3 },
   ],
 };
 
-// mode가 Y이면, 탈퇴하기 버튼 노출하고 클릭 시 status N으로 변경
+// mode가 Y일때, admin(생성자)가 본인이면 설정하기, 삭제하기 버튼 노출
+// -> 설정하기 클릭 시 팀 설정 팝업 / 삭제하기 버튼 클릭 시 team[index] 삭제
+// mode가 Y일때, admin(생성자)가 본인이 아니면 탈퇴하기 버튼 노출하고 클릭 시 status N으로 변경
 // mode가 N이면, 가입하기, 거부하기 버튼 노출
-// 가입하기 클릭 시 status Y로 변경 / 거부하기 클릭 시 user.team에서 삭제
-function ButtonList(props) {
-  if (props === 'Y') {
-    return (
-      <div>
-        <button type="button">탈퇴하기</button>
-      </div>
-    );
+// -> 가입하기 클릭 시 status Y로 변경 / 거부하기 클릭 시 user.team에서 삭제 및 team[index].coworker에서 삭제
+
+function ButtonList(props: { status: string; id: number; admin: number }) {
+  const [mode, setMode] = useState(props.status);
+  const [teamList, setTeamList] = useState([
+    { id: 1, name: 'GU-99', admin: 1, coworker: [2, 3], desc: '' },
+    { id: 2, name: '김찌와 쏘주', admin: 3, coworker: [1], desc: '' },
+    { id: 3, name: '모코코', admin: 3, coworker: [1, 2], desc: '' },
+  ]);
+
+  const [coworker, setCoworker] = useState(teamList[Number(props.id)]);
+  const [status, setStatus] = useState(props.status);
+  const newTeam = {
+    id: props.id,
+    name: teamList[Number(props.id)].name,
+    admin: props.admin,
+    coworker: teamList[Number(props.id)].coworker,
+    desc: teamList[Number(props.id)].desc,
+  };
+  const newTeamList = [...teamList];
+  newTeamList.push(newTeam);
+  setTeamList(newTeamList);
+
+  if (props.status === 'Y') {
+    if (props.id === props.admin) {
+      return (
+        <div>
+          <button type="button">설정하기</button>
+          <button type="button">삭제하기</button>
+        </div>
+      );
+    }
+    if (props.id !== props.admin) {
+      return (
+        <div>
+          <button type="button">탈퇴하기</button>
+        </div>
+      );
+    }
   }
-  if (props === 'N') {
+  if (props.status === 'N') {
     return (
       <div>
-        <button type="button">가입하기</button>
+        <button
+          type="button"
+          value="Y"
+          onClick={(event) => setMode(event.currentTarget.value)}
+          onChange={(event) => setStatus(event.currentTarget.value)}
+        >
+          가입하기
+        </button>
         <button type="button">거부하기</button>
       </div>
     );
   }
 }
 
-function TeamList(props) {
+function TeamList(props: { status: string }) {
   const list = [];
-  for (let i = 0; i < user.team.length; i) {
+  for (let i = 0; i < user.team.length; i += 1) {
     if (user.team[i].status === props.status) {
-      list.push(<div>{user.team[i].name}</div>);
+      list.push(
+        <div className="flex">
+          <div>{user.team[i].name}</div>
+          <ButtonList status={props.status} id={user.id} admin={user.team[i].admin} />
+        </div>,
+      );
     }
   }
   return list;
@@ -64,7 +110,6 @@ function TeamSettingPage() {
         </button>
       </nav>
       <TeamList status={mode} />
-      <ButtonList status={mode} />
     </>
   );
 }
