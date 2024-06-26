44d596ea7e946412523ea26cda89496e702426b8
Seok93 <gkdltjr@naver.com>
2024-06-26 21:05:53 +0900

    fix: #24 falsy 값 출력 방지를 위한 간단한 로직 변경

--- src/components/sidebar/ListProject.tsx
+++ src/components/sidebar/ListProject.tsx
@@ -14,7 +14,7 @@ export default function ListProject({ data, targetId }: ListProjectProps) {
       {data.map((item) => (
         <li
           key={item.id}
-          className={`relative cursor-pointer border-b bg-white hover:brightness-90 ${targetId === item.id && 'selected'}`}
+          className={`relative cursor-pointer border-b bg-white hover:brightness-90 ${targetId === item.id ? 'selected' : ''}`}
         >
           <Link
             to={`/teams/${teamId}/projects/${item.id}/calendar`}
