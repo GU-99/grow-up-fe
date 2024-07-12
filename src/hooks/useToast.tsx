3d17bea6da44101e238c8c526a60540244d544bb
Seok93 <gkdltjr@naver.com>
2024-07-12 17:12:01 +0900

    UI: #52 토스트 메세지별 border-bottom-color 설정

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -3,10 +3,10 @@ import { toast } from 'react-toastify';
 type ToastPromise = <T>(promise: Promise<T> | (() => Promise<T>), target: string) => Promise<T>;
 
 export default function useToast() {
-  const toastInfo = (message: string) => toast.info(message);
-  const toastSuccess = (message: string) => toast.success(message);
-  const toastError = (message: string) => toast.error(message);
-  const toastWarn = (message: string) => toast.warn(message);
+  const toastInfo = (message: string) => toast.info(message, { className: 'border-b-4 border-[#3498db]' });
+  const toastSuccess = (message: string) => toast.success(message, { className: 'border-b-4 border-[#07bc0c]' });
+  const toastError = (message: string) => toast.error(message, { className: 'border-b-4 border-[#e74c3c]' });
+  const toastWarn = (message: string) => toast.warn(message, { className: 'border-b-4 border-[#f1c40f]' });
   const toastPromise: ToastPromise = (promise, target) => {
     return toast.promise(promise, {
       pending: `${target} 처리 진행중...`,
