d716c418e94d679d08d25654917be09d3e37c923
Seok93 <gkdltjr@naver.com>
2024-07-12 16:41:09 +0900

    Chore: #52 불필요한 타입 제거 및 멀티 라인 명시적 그룹화

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -7,12 +7,13 @@ export default function useToast() {
   const toastSuccess = (message: string) => toast.success(message);
   const toastError = (message: string) => toast.error(message);
   const toastWarn = (message: string) => toast.warn(message);
-  const toastPromise: ToastPromise = (promise, target: string) =>
-    toast.promise(promise, {
+  const toastPromise: ToastPromise = (promise, target) => {
+    return toast.promise(promise, {
       pending: `${target} 처리 진행중...`,
       success: `${target} 처리 성공`,
       error: `${target} 처리 실패`,
     });
+  };
 
   return { toastInfo, toastSuccess, toastError, toastWarn, toastPromise };
 }
