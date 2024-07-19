e3971f81f4f8359d69355dbd75b866887e8b33e8
Seok93 <gkdltjr@naver.com>
2024-07-19 15:08:50 +0900

    UI: #51 DateHeader 커스텀 디자인 추가

--- src/components/task/calendar/CustomDateHeader.tsx
+++ src/components/task/calendar/CustomDateHeader.tsx
@@ -1,8 +1,24 @@
+import { useMemo } from 'react';
+import { DateTime } from 'luxon';
 import { DateHeaderProps } from 'react-big-calendar';
+import { LuxonWeekday } from '@constants/date';
 
-// ToDo: Header 컴포넌트 수정할 것
-export default function CustomDateHeader({ date, drilldownView, isOffRange, label, onDrillDown }: DateHeaderProps) {
-  console.log(date, drilldownView, isOffRange, label, onDrillDown);
-  console.log(date.getDate());
-  return <div>{date.getDate()}</div>;
+function getTextColor(weekday: LuxonWeekday, isOffRange: boolean) {
+  if (isOffRange) return 'text-[#999999]';
+
+  switch (weekday) {
+    case LuxonWeekday.SATURDAY:
+      return 'text-[#000AFF]';
+    case LuxonWeekday.SUNDAY:
+      return 'text-[#FF0000]';
+    default:
+      return 'text-default';
+  }
+}
+
+export default function CustomDateHeader({ date, isOffRange }: DateHeaderProps) {
+  const { weekday } = useMemo(() => DateTime.fromJSDate(date), [date]);
+  const textColor = useMemo(() => getTextColor(weekday, isOffRange), [weekday, isOffRange]);
+
+  return <div className={`pl-3 text-left ${textColor}`}>{date.getDate()}</div>;
 }
