994aa5822c906a90d0800d4d2047182b3ca3af2c
yoonyesol <maniac970430@naver.com>
2024-07-27 18:09:15 +0900

    Refactor: #35 폼 하단 공통 링크 컴포넌트 중복제거 리팩토링

--- src/components/user/authForm/FooterLinks.tsx
+++ src/components/user/authForm/FooterLinks.tsx
@@ -4,64 +4,48 @@ type FooterLinksProps = {
   type: 'signIn' | 'searchId' | 'searchPassword';
 };
 
+const texts = {
+  signIn: '로그인',
+  findId: '아이디 찾기',
+  findPassword: '비밀번호 찾기',
+};
+
+const paths = {
+  signIn: '/signin',
+  findId: '/search/id',
+  findPassword: '/search/password',
+};
+
+const links = {
+  signIn: [
+    { text: texts.findId, path: paths.findId },
+    { text: texts.findPassword, path: paths.findPassword },
+  ],
+  searchId: [
+    { text: texts.signIn, path: paths.signIn },
+    { text: texts.findPassword, path: paths.findPassword },
+  ],
+  searchPassword: [
+    { text: texts.signIn, path: paths.signIn },
+    { text: texts.findId, path: paths.findId },
+  ],
+};
+
 export default function FooterLinks({ type }: FooterLinksProps) {
   const nav = useNavigate();
 
-  const renderLinks = () => {
-    switch (type) {
-      case 'signIn':
-        return (
-          <>
-            <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/search/id')}>
-              아이디 찾기
-            </button>
-            <p>|</p>
-            <button
-              type="button"
-              className="cursor-pointer bg-inherit font-bold"
-              onClick={() => nav('/search/password')}
-            >
-              비밀번호 찾기
-            </button>
-          </>
-        );
-      case 'searchId':
-        return (
-          <>
-            <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/signin')}>
-              로그인
-            </button>
-            <p>|</p>
-            <button
-              type="button"
-              className="cursor-pointer bg-inherit font-bold"
-              onClick={() => nav('/search/password')}
-            >
-              비밀번호 찾기
-            </button>
-          </>
-        );
-      case 'searchPassword':
-        return (
-          <>
-            <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/signin')}>
-              로그인
-            </button>
-            <p>|</p>
-            <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/search/id')}>
-              아이디 찾기
-            </button>
-          </>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
     <>
-      <div className="flex flex-row justify-center gap-8">{renderLinks()}</div>
-
+      <div className="flex flex-row justify-center">
+        {links[type].map((link, index) => (
+          <div key={link.text} className="flex flex-row">
+            <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav(link.path)}>
+              {link.text}
+            </button>
+            {index < links[type].length - 1 && <p className="mx-8">|</p>}
+          </div>
+        ))}
+      </div>
       <div className="mb-35 mt-15 flex flex-row items-center justify-center gap-8">
         <p className="items-center font-bold">회원이 아니신가요?</p>
         <button type="button" className="auth-btn" onClick={() => nav('/signup')}>
