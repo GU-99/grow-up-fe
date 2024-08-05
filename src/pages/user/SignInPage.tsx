b2565e59ce58ff9b27a3c2a096c446521f6fba25
Yoonyesol <51500821+Yoonyesol@users.noreply.github.com>
2024-08-05 17:32:59 +0900

    Feat: #35 ID/PW 찾기 페이지 UI 작성 및 Validation 체크 (#58)
    * Feat: #35 ID 찾기 페이지 마크업 및 스타일링
    
    * Feat: #35 PW 찾기 페이지 마크업 및 스타일링
    
    * Refactor: #35 로그인, ID, PW 찾기 페이지 중복 추출 컴포넌트 제작 및 적용
    
    * Refactor: #35 인증 페이지 전반적으로 겹치는 스타일링 분리
    
    * Formatting: #35 로그인 페이지의 email 필드를 id 필드로 변경
    
    * UI: #35 공용 버튼에서 그림자 제거
    
    * UI: #35 로그인, ID/PW 찾기 페이지 리뷰 반영 디자인 수정
    
    * Feat: #35 닉네임 validation 추가 및 기존 validation, 정규식 관련 코드 수정
    
    * Refactor: #35 폼 하단 공통 링크 컴포넌트 중복제거 리팩토링
    
    * Refactor: #35 회원가입 페이지 비밀번호 확인 validate 분리 및 공용Form 컴포넌트 props 타입 수정

--- src/pages/user/SignInPage.tsx
+++ src/pages/user/SignInPage.tsx
@@ -1,13 +1,13 @@
-import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import Kakao from '@assets/social_kakao_icon.svg';
 import Google from '@assets/social_google_icon.svg';
-import { UserSignIn } from '@/types/UserType';
+import { UserSignInForm } from '@/types/UserType';
 import ValidationInput from '@/components/common/ValidationInput';
 import { STATUS_VALIDATION_RULES } from '@/constants/formValidationRules';
+import AuthForm from '@/components/user/authForm/AuthForm';
+import FooterLinks from '@/components/user/authForm/FooterLinks';
 
 export default function SignInPage() {
-  const nav = useNavigate();
   const {
     register,
     handleSubmit,
@@ -15,28 +15,23 @@ export default function SignInPage() {
   } = useForm({
     mode: 'onChange',
     defaultValues: {
-      email: '',
+      id: '',
       password: '',
     },
   });
 
-  const onSubmit = (data: UserSignIn) => {
+  const onSubmit = (data: UserSignInForm) => {
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} noValidate className="flex h-screen w-300 flex-col py-30">
-      <section className="h-1/6 text-large text-main">
-        Welcome to our site!
-        <br /> Grow Up your Life with us.
-      </section>
-
-      <section className="flex flex-grow flex-col justify-center gap-8">
-        {/* 이메일(아이디) */}
+    <>
+      <AuthForm onSubmit={handleSubmit(onSubmit)} styles="mt-40">
+        {/* 아이디 */}
         <ValidationInput
-          placeholder="이메일"
-          errors={errors.email?.message}
-          register={register('email', STATUS_VALIDATION_RULES.EMAIL())}
+          placeholder="아이디"
+          errors={errors.id?.message}
+          register={register('id', STATUS_VALIDATION_RULES.ID)}
         />
 
         {/* 비밀번호 */}
@@ -44,47 +39,31 @@ export default function SignInPage() {
           placeholder="비밀번호 (영문자, 숫자, 기호 포함 8~16자리)"
           type="password"
           errors={errors.password?.message}
-          register={register('password', STATUS_VALIDATION_RULES.PASSWORD())}
+          register={register('password', STATUS_VALIDATION_RULES.PASSWORD)}
         />
 
-        <div className="flex flex-col gap-4 text-center">
+        <div className="flex flex-col text-center">
           <button type="submit" className="auth-btn" disabled={isSubmitting}>
             로그인
           </button>
         </div>
 
-        <div className="flex flex-row justify-center gap-8">
-          <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/search/id')}>
-            아이디 찾기
-          </button>
-          <p>|</p>
-          <button type="button" className="cursor-pointer bg-inherit font-bold" onClick={() => nav('/search/password')}>
-            비밀번호 찾기
-          </button>
-        </div>
-
-        <div className="mb-35 mt-15 flex flex-row items-center justify-center gap-8">
-          <p className="items-center font-bold">회원이 아니신가요?</p>
-          <button type="button" className="auth-btn" onClick={() => nav('/signup')}>
-            회원가입
-          </button>
-        </div>
-      </section>
-
-      <section className="flex h-1/6 flex-col gap-4 text-center">
+        <FooterLinks type="signIn" />
+      </AuthForm>
+      <section className="flex h-1/6 flex-col gap-8 text-center">
         <button type="button" className="auth-btn bg-kakao" disabled={isSubmitting}>
-          <div className="flex w-81 items-center justify-between">
+          <div className="flex h-30 w-81 items-center justify-between">
             <img src={Kakao} alt="Kakao" className="size-15" />
             카카오 로그인
           </div>
         </button>
         <button type="button" className="auth-btn bg-button" disabled={isSubmitting}>
-          <div className="flex w-81 items-center justify-between">
+          <div className="flex h-30 w-81 items-center justify-between">
             <img src={Google} alt="Google" className="size-42" />
             로그인
           </div>
         </button>
       </section>
-    </form>
+    </>
   );
 }
