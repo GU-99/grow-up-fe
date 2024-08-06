f604a9cb6381a25025da5c29ccdaaf621353aea7
yoonyesol <maniac970430@naver.com>
2024-08-06 23:23:42 +0900

    Refactor: #65 Timer 컴포넌트 리뷰 반영 리팩토링

--- src/components/common/Timer.tsx
+++ src/components/common/Timer.tsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
 
-export default function Timer({ time, onTimeout }: { time: number; onTimeout: () => void }) {
+type TimerProps = {
+  time: number;
+  onTimeout: () => void;
+};
+
+export default function Timer({ time, onTimeout }: TimerProps) {
   const [timeLeft, setTimeLeft] = useState(time);
 
   useEffect(() => {
@@ -16,9 +21,11 @@ export default function Timer({ time, onTimeout }: { time: number; onTimeout: ()
   }, [timeLeft, onTimeout]);
 
   const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    const minutes = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, '0');
+    const remainingSeconds = (seconds % 60).toString().padStart(2, '0');
+    return `${minutes}:${remainingSeconds}`;
   };
 
   return <p className="text-sm font-regular text-error">{formatTime(timeLeft)}</p>;
