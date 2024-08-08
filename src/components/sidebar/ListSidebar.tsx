2495ed4c36a5f263366f0f29d54310e35c607a24
seungchanwoo <tmdcksqkd123@naver.com>
2024-08-08 15:29:24 +0900

    Comment: #62 불필요한 주석 삭제

--- src/components/sidebar/ListSidebar.tsx
+++ src/components/sidebar/ListSidebar.tsx
@@ -9,7 +9,6 @@ type ListSidebarProps = {
   onClick?: () => void;
 };
 
-// ToDo: 프로젝트 생성 등과 같은 버튼 기능 추가할 것
 export default function ListSidebar({ label, title, children, showButton, text, onClick }: ListSidebarProps) {
   return (
     <aside className="mr-10 flex w-1/3 flex-col border border-list bg-contents-box">
@@ -18,7 +17,6 @@ export default function ListSidebar({ label, title, children, showButton, text, 
           {label && <small className="mr-5 font-bold text-main">{label}</small>}
           <span className="text-emphasis">{title}</span>
         </div>
-        {/* 팀 생성 모달 */}
         {showButton && (
           <button
             type="button"
