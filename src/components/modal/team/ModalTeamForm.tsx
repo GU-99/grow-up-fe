1b553760db2eeb51b65f9110e0cf739564426678
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-08-08 16:14:41 +0900

    UI: #62 모든 프로젝트 관리 페이지 중 팀/프로젝트가 없는 경우 UI 작성 (#64)
    * UI: #62 모든 프로젝트 관리 페이지중 팀/프로젝트가 없는 경우 UI 작성
    
    * Feat: #62 teamid 예외처리 추가 및 ListSidebar button props 변경
    
    * Formatting: #62 ListSidebar props 이름 변경
    
    * Comment: #62 불필요한 주석 삭제
    
    * Formatting: #62 오탈자 수정
    
    * Formatting: #62 import 경로 정리

---
+++ src/components/modal/team/ModalTeamForm.tsx
@@ -0,0 +1,19 @@
+import { useForm } from 'react-hook-form';
+
+import type { SubmitHandler } from 'react-hook-form';
+import type { Team } from '@/types/TeamType';
+
+type ModalTeamFormProps = {
+  formId: string;
+  onSubmit: SubmitHandler<Team>;
+};
+
+export default function ModalTeamForm({ formId, onSubmit }: ModalTeamFormProps) {
+  const { handleSubmit } = useForm<Team>();
+
+  return (
+    <form id={formId} className="mb-10 flex grow flex-col justify-center" onSubmit={handleSubmit(onSubmit)}>
+      123123123
+    </form>
+  );
+}
