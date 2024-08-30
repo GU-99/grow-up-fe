b60bb32c0ee11ca69b3a00d19a9d2b4cc684388e
Seok93 <gkdltjr@naver.com>
2024-08-30 15:33:11 +0900

    Feat: #98 MSW 프로젝트 상태 목록 조회 API handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,14 @@ import userServiceHandler from '@mocks/services/userServiceHandler';
 import teamServiceHandler from '@mocks/services/teamServiceHandler';
 import projectServiceHandler from '@mocks/services/projectServiceHandler';
 import taskServiceHandler from '@mocks/services/taskServiceHandler';
+import statusServiceHandler from '@mocks/services/statusServiceHandler';
 
-const handlers = [...userServiceHandler, ...teamServiceHandler, ...projectServiceHandler, ...taskServiceHandler];
+const handlers = [
+  ...userServiceHandler,
+  ...teamServiceHandler,
+  ...projectServiceHandler,
+  ...taskServiceHandler,
+  ...statusServiceHandler,
+];
 
 export default handlers;
