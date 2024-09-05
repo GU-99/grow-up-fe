bd4382a6df316131b3a58e6e38d04ae83fc352bd
Suk Woo <gkdltjr@naver.com>
2024-09-05 16:00:17 +0900

    UI: #107 모달 레이아웃 UI 수정 (#108)
    * UI: #107 모달 레이아웃 & 모달 버튼 컴포넌트 UI 수정
    
    * UI: #107 프로젝트 상태 Form UI 수정

--- src/layouts/ModalLayout.tsx
+++ src/layouts/ModalLayout.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { IoMdCloseCircle } from 'react-icons/io';
 
 type ModalLayoutProps = {
   onClose: () => void;
@@ -15,12 +16,22 @@ export default function ModalLayout({ onClose, children }: ModalLayoutProps) {
     return () => globalThis.removeEventListener('keydown', handleKeyDownClose);
   }, []);
 
+  const handleCloseClick = () => onClose();
+
   return (
-    <div
+    <section
       className="absolute bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center bg-black/50"
       tabIndex={-1}
     >
-      <div className="flex h-4/5 max-h-375 w-375 flex-col items-center overflow-auto bg-white p-20">{children}</div>
-    </div>
+      <article className="flex h-4/5 max-h-375 w-375 flex-col items-center overflow-auto bg-white p-20">
+        <header className="flex w-full justify-end">
+          <IoMdCloseCircle
+            className="size-12 cursor-pointer text-close hover:text-[#DF0000]"
+            onClick={handleCloseClick}
+          />
+        </header>
+        <div className="flex w-full grow flex-col items-center *:w-4/5">{children}</div>
+      </article>
+    </section>
   );
 }
