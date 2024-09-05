30faf3592f8109d2098190fc88e68d9021f60c87
yoonyesol <maniac970430@naver.com>
2024-09-06 01:42:15 +0900

    Feat: #80 로그인 API 호출 시 useAxios 훅 적용

--- src/pages/user/SignInPage.tsx
+++ src/pages/user/SignInPage.tsx
@@ -6,16 +6,19 @@ import { USER_AUTH_VALIDATION_RULES } from '@constants/formValidationRules';
 import FooterLinks from '@components/user/auth-form/FooterLinks';
 import AuthFormLayout from '@layouts/AuthFormLayout';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { AxiosError } from 'axios';
 import useToast from '@hooks/useToast';
 import { login } from '@services/authService';
+import { useEffect } from 'react';
 import type { UserSignInForm } from '@/types/UserType';
 import { useAuthStore } from '@/stores/useAuthStore';
+import useAxios from '@/hooks/useAxios';
 
 export default function SignInPage() {
   const { onLogin } = useAuthStore();
   const { toastError } = useToast();
   const navigate = useNavigate();
+  const { error, fetchData, headers, loading } = useAxios(login);
 
   const {
     register,
@@ -29,23 +32,31 @@ export default function SignInPage() {
     },
   });
 
-  const onSubmit = async (data: UserSignInForm) => {
-    try {
-      const response = await login(data);
-      const accessToken = response.headers.authorization;
-      if (!accessToken) return toastError('로그인에 실패했습니다.');
-
-      const token = accessToken.split(' ')[1];
-      onLogin(token);
-
-      navigate('/', { replace: true });
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response?.status === 401) {
-        return toastError('아이디와 비밀번호를 한번 더 확인해 주세요.');
-      }
-      return toastError(`로그인 도중 오류가 발생했습니다: ${error}`);
-    }
+  const onSubmit = async (formData: UserSignInForm) => {
+    await fetchData(formData);
   };
+
+  useEffect(() => {
+    if (headers) {
+      const accessToken = headers.authorization;
+      if (!accessToken) {
+        toastError('로그인에 실패했습니다.');
+        return;
+      }
+
+      onLogin(accessToken.split(' ')[1]);
+      navigate('/', { replace: true });
+      return;
+    }
+
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        toastError('아이디와 비밀번호를 한번 더 확인해 주세요.');
+        return;
+      }
+      toastError(`로그인 도중 오류가 발생했습니다: ${error.message}`);
+    }
+  }, [headers, error]);
 
   return (
     <>
