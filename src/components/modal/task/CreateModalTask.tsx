810c989c781034724dddf885405febf3656fc6ef
Seok93 <gkdltjr@naver.com>
2024-09-10 15:18:45 +0900

    Feat: #114 일정 생성 기능 구현

--- src/components/modal/task/CreateModalTask.tsx
+++ src/components/modal/task/CreateModalTask.tsx
@@ -2,10 +2,13 @@ import ModalLayout from '@layouts/ModalLayout';
 import ModalPortal from '@components/modal/ModalPortal';
 import ModalTaskForm from '@components/modal/task/ModalTaskForm';
 import ModalFormButton from '@components/modal/ModalFormButton';
+import { useCreateStatusTask, useReadStatusTasks } from '@hooks/query/useTaskQuery';
 
 import type { SubmitHandler } from 'react-hook-form';
 import type { TaskForm } from '@/types/TaskType';
 import type { Project } from '@/types/ProjectType';
+import { ProjectStatus } from '@/types/ProjectStatusType';
+import useToast from '@/hooks/useToast';
 
 type CreateModalTaskProps = {
   project: Project;
@@ -13,10 +16,23 @@ type CreateModalTaskProps = {
 };
 
 export default function CreateModalTask({ project, onClose: handleClose }: CreateModalTaskProps) {
-  // ToDo: 상태 생성을 위한 네트워크 로직 추가
-  const handleSubmit: SubmitHandler<TaskForm> = async (data) => {
-    console.log('생성 폼 제출');
-    console.log(data);
+  const { toastError } = useToast();
+  const { mutate: createTaskMutate } = useCreateStatusTask(project.projectId);
+  const { statusTaskList } = useReadStatusTasks(project.projectId);
+
+  const getLastSortOrder = (statusId: ProjectStatus['statusId']) => {
+    const statusTask = statusTaskList.find((statusTask) => statusTask.statusId === Number(statusId));
+    if (!statusTask) {
+      toastError('선택하신 프로젝트 상태는 존재하지 않습니다. ');
+      throw Error('프로젝트 상태가 존재하지 않습니다.');
+    }
+    return statusTask.tasks.length + 1;
+  };
+
+  // ToDo: 파일 생성 위한 네트워크 로직 추가
+  const handleSubmit: SubmitHandler<TaskForm> = async (taskFormData) => {
+    const sortOrder = getLastSortOrder(taskFormData.statusId);
+    createTaskMutate({ ...taskFormData, sortOrder });
     handleClose();
   };
   return (
