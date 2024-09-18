5501ce78eeec8a9830201fb867b9063ff257d6f4
yoonyesol <maniac970430@naver.com>
2024-09-18 17:28:13 +0900

    UI: #137 비밀번호 찾기 Form UI 수정 및 결과 화면 UI 작성

--- src/pages/user/SearchPasswordPage.tsx
+++ src/pages/user/SearchPasswordPage.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import AuthFormLayout from '@layouts/AuthFormLayout';
 import { USER_AUTH_VALIDATION_RULES } from '@constants/formValidationRules';
 import ValidationInput from '@components/common/ValidationInput';
 import FooterLinks from '@components/user/auth-form/FooterLinks';
+import VerificationButton from '@components/user/auth-form/VerificationButton';
+import useEmailVerification from '@hooks/useEmailVerification';
+import AuthFormLayout from '@layouts/AuthFormLayout';
 import type { SearchPasswordForm } from '@/types/UserType';
 
 export default function SearchPasswordPage() {
+  const { isVerificationRequested, requestVerificationCode, expireVerificationCode } = useEmailVerification();
+  // const [tempPassword, setTempPassword] = useState(null);
+  const [tempPassword, settempPassword] = useState('abce@qwer');
+  const nav = useNavigate();
   const {
     register,
     handleSubmit,
@@ -25,34 +33,56 @@ export default function SearchPasswordPage() {
 
   return (
     <AuthFormLayout onSubmit={handleSubmit(onSubmit)} marginTop="mt-34.9">
-      {/* 아이디 */}
-      <ValidationInput
-        placeholder="아이디"
-        errors={errors.username?.message}
-        register={register('username', USER_AUTH_VALIDATION_RULES.ID)}
-      />
+      {tempPassword ? (
+        <div className="mt-90 h-full space-y-20">
+          <div className="space-y-5 text-center">
+            <p className="text-sm">임시 비밀번호로 로그인해주세요.</p>
+            <p>
+              임시 비밀번호: <b>{tempPassword}</b>
+            </p>
+          </div>
+          <button type="button" className="auth-btn w-full" onClick={() => nav('/signin')}>
+            로그인으로 돌아가기
+          </button>
+        </div>
+      ) : (
+        <>
+          {/* 아이디 */}
+          <ValidationInput
+            placeholder="아이디"
+            errors={errors.username?.message}
+            register={register('username', USER_AUTH_VALIDATION_RULES.ID)}
+          />
 
-      {/* 이메일 */}
-      <ValidationInput
-        isButtonInput
-        buttonLabel="인증번호 발송"
-        placeholder="이메일"
-        errors={errors.email?.message}
-        register={register('email', USER_AUTH_VALIDATION_RULES.EMAIL)}
-      />
+          {/* 이메일 */}
+          <ValidationInput
+            placeholder="이메일"
+            errors={errors.email?.message}
+            register={register('email', USER_AUTH_VALIDATION_RULES.EMAIL)}
+          />
 
-      {/* 이메일 인증 */}
-      <ValidationInput
-        placeholder="인증번호"
-        errors={errors.code?.message}
-        register={register('code', USER_AUTH_VALIDATION_RULES.CERTIFICATION)}
-      />
+          {/* 이메일 인증 */}
+          {isVerificationRequested && (
+            <ValidationInput
+              placeholder="인증번호"
+              errors={errors.code?.message}
+              register={register('code', USER_AUTH_VALIDATION_RULES.CERTIFICATION)}
+            />
+          )}
 
-      <button type="submit" className="auth-btn" disabled={isSubmitting}>
-        비밀번호 찾기
-      </button>
-
-      <FooterLinks type="searchPassword" />
+          {/* 인증 요청 및 확인 버튼 */}
+          <div className="space-y-8 text-center">
+            <VerificationButton
+              isVerificationRequested={isVerificationRequested}
+              isSubmitting={isSubmitting}
+              requestCode={handleSubmit(requestVerificationCode)}
+              expireVerificationCode={expireVerificationCode}
+              buttonLabel="비밀번호 찾기"
+            />
+          </div>
+          <FooterLinks type="searchPassword" />
+        </>
+      )}
     </AuthFormLayout>
   );
 }
