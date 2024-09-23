a406bf6c7afc3012a4e5d2b57336280bfa98f8ad
Seok93 <gkdltjr@naver.com>
2024-09-23 21:14:00 +0900

    Feat: #147 프로젝트 팀원 목록 조회 React Query 처리 추가

--- src/hooks/query/useProjectQuery.ts
+++ src/hooks/query/useProjectQuery.ts
@@ -1,10 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import { getProjectList } from '@services/projectService';
+import { getProjectList, getProjectUserRoleList } from '@services/projectService';
 
 import type { Team } from '@/types/TeamType';
+import type { Project } from '@/types/ProjectType';
 
 // Todo: Project Query CUD로직 작성하기
-export default function useReadProjects(teamId: Team['teamId']) {
+// 팀에 속한 프로젝트 목록 조회
+export function useReadProjects(teamId: Team['teamId']) {
   const {
     data: projectList = [],
     isLoading: isProjectLoading,
@@ -19,4 +21,22 @@ export default function useReadProjects(teamId: Team['teamId']) {
   });
 
   return { projectList, isProjectLoading, isProjectError, projectError };
+}
+
+// 프로젝트 팀원 목록 조회
+export function useReadProjectUserRoleList(projectId: Project['projectId']) {
+  const {
+    data: projectUserRoleList = [],
+    isLoading: isProjectUserRoleLoading,
+    isError: isErrorProjectUserRole,
+    error: projectUserRoleError,
+  } = useQuery({
+    queryKey: ['projects', projectId, 'users'],
+    queryFn: async () => {
+      const { data } = await getProjectUserRoleList(projectId);
+      return data;
+    },
+  });
+
+  return { projectUserRoleList, isProjectUserRoleLoading, isErrorProjectUserRole, projectUserRoleError };
 }
