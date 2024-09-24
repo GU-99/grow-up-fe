a38c4151637ce6e8e104e5449707f03b10aee6b0
Seok93 <gkdltjr@naver.com>
2024-09-24 15:29:40 +0900

    Remove: #148 Assignee 타입 삭제 UserWithRole 타입으로 대체

--- src/types/AssigneeType.tsx
+++
@@ -1,8 +0,0 @@
-import type { User } from '@/types/UserType';
-import type { Role } from '@/types/RoleType';
-
-export type Assignee = {
-  userId: User['userId'];
-  nickname: User['nickname'];
-  roleName: Role['roleName'];
-};
