3013c08e744dd7401a77f2c5c5799670645acdac
Seok93 <gkdltjr@naver.com>
2024-09-27 16:30:37 +0900

    Feat: #115 파일 Validation 추가

--- src/utils/Validator.ts
+++ src/utils/Validator.ts
@@ -1,4 +1,5 @@
 import { DateTime } from 'luxon';
+import { FILE_NAME_REGEX } from '@constants/regex';
 
 export default class Validator {
   public static isEmptyString(value: string) {
@@ -25,5 +26,29 @@ export default class Validator {
     const startDate = DateTime.fromJSDate(new Date(start));
     const endDate = DateTime.fromJSDate(new Date(end));
     return startDate <= endDate;
+  }
+
+  public static isPositiveNumberWithZero(num: number) {
+    return typeof num === 'number' && num >= 0;
+  }
+
+  public static isValidFileCount(maxCount: number, currentCount: number) {
+    if (!Validator.isPositiveNumberWithZero(maxCount) || !Validator.isPositiveNumberWithZero(currentCount)) {
+      throw Error('최대 파일수와 현재 파일 수는 음수가 될 수 없습니다.');
+    }
+    return maxCount >= currentCount;
+  }
+
+  public static isValidFileSize(maxSize: number, fileSize: number) {
+    return maxSize >= fileSize;
+  }
+
+  public static isValidFileExtension(acceptFileTypes: string[], fileType: string) {
+    return acceptFileTypes.some((acceptFileType) => acceptFileType === fileType);
+  }
+
+  public static isValidFileName(fileName: string) {
+    const fileNameRegex = new RegExp(FILE_NAME_REGEX);
+    return fileNameRegex.test(fileName);
   }
 }
