ee2bb695303245893df972b9b031e5e5004bf154
yoonyesol <maniac970430@naver.com>
2024-10-04 01:05:47 +0900

    Refactor: #175 queryKeyGenergator를 이용해 쿼리 키 생성 및 적용

--- src/hooks/query/useUserQuery.ts
+++ src/hooks/query/useUserQuery.ts
@@ -1,18 +1,20 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { patchUserInfo } from '@services/userService';
 import useToast from '@hooks/useToast';
+import { patchUserInfo } from '@services/userService';
+import { generateUserInfoQueryKey } from '@utils/queryKeyGenergator';
 import type { EditUserInfoRequest } from '@/types/UserType';
 
 export function useUpdateUserInfo() {
   const queryClient = useQueryClient();
   const { toastSuccess, toastError } = useToast();
+  const userInfoQueryKey = generateUserInfoQueryKey();
 
   const mutation = useMutation({
     mutationFn: (data: EditUserInfoRequest) => patchUserInfo(data),
     onError: () => toastError('유저 정보 수정에 실패했습니다. 다시 시도해 주세요.'),
     onSuccess: () => {
       toastSuccess('유저 정보가 수정되었습니다.');
-      queryClient.invalidateQueries({ queryKey: ['userInfo'] });
+      queryClient.invalidateQueries({ queryKey: userInfoQueryKey });
     },
   });
 
