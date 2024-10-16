dd4bca770687adf241bcaddb6742c43799529a09
seungchanwoo <tmdcksqkd123@naver.com>
2024-10-16 21:13:43 +0900

    UI:#199 프로젝트 생성 UI 구현

--- src/types/ProjectType.tsx
+++ src/types/ProjectType.tsx
@@ -1,3 +1,7 @@
+import type { User } from '@/types/UserType';
+import type { ProjectRoleName } from '@/types/RoleType';
+
+// ToDo: API 설계 완료시 데이터 타입 변경할 것
 export type Project = {
   projectId: number;
   teamId: number;
@@ -5,4 +9,16 @@ export type Project = {
   content: string;
   startDate: Date | null;
   endDate: Date | null;
+};
+
+export type ProjectCoworker = {
+  userId: User['userId'];
+  roleName: ProjectRoleName;
+  nickname?: string;
+};
+
+export type ProjectCoworkerInfo = ProjectCoworker & { nickname: User['nickname'] };
+
+export type ProjectForm = Omit<Project, 'projectId' | 'teamId'> & {
+  coworkers: ProjectCoworker[];
 };
