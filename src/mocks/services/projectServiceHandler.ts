382fe2a45ae45bfc9590869d0c49398e24d97735
Suk Woo <gkdltjr@naver.com>
2024-10-19 15:28:42 +0900

    Refactor: #224 MSW 처리중 StatusService, ProjectService 기능 리팩토링 (#226)
    * Refactor: #224 MSW StatusService 기능 리팩토링
    
    * Formatting: #224 일정 수정 API 로직중 폼 데이터 변수명 수정
    
    * Refactor: #224 MSW ProjectSevice 기능 리팩토링
    
    * Fix: #224 프로젝트 관련 정보 조회성 로직은 팀에 속하는가로 수정
    
    * Fix: #224 팀 접근 권한에 넘기는 매개변수 수정
    
    * Fix: #224 AI 리뷰 반영

--- src/mocks/services/projectServiceHandler.ts
+++ src/mocks/services/projectServiceHandler.ts
@@ -1,13 +1,24 @@
 import { http, HttpResponse } from 'msw';
 import {
-  PROJECT_DUMMY,
-  PROJECT_USER_DUMMY,
-  STATUS_DUMMY,
-  TASK_DUMMY,
-  TASK_FILE_DUMMY,
-  TASK_USER_DUMMY,
-} from '@mocks/mockData';
-import { getRoleHash, getUserHash } from '@mocks/mockHash';
+  deleteAllProjectStatus,
+  deleteAllProjectUser,
+  deleteAllTask,
+  deleteAllTaskFile,
+  deleteAllTaskFileInMemory,
+  deleteAllTaskUser,
+  deleteProject,
+  findAllProject,
+  findAllProjectStatus,
+  findAllProjectUser,
+  findAllTask,
+  findProject,
+  findProjectUser,
+  findRole,
+  findTeamUser,
+  findUser,
+} from '@mocks/mockAPI';
+import { convertTokenToUserId } from '@utils/converter';
+import type { SearchUser, UserWithRole } from '@/types/UserType';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -15,115 +26,137 @@ const projectServiceHandler = [
   // 프로젝트 소속 유저 검색 API
   http.get(`${BASE_URL}/project/:projectId/user/search`, ({ request, params }) => {
     const url = new URL(request.url);
-    const nickname = url.searchParams.get('nickname');
+    const nickname = url.searchParams.get('nickname') || '';
     const accessToken = request.headers.get('Authorization');
-    const { projectId } = params;
+    const projectId = Number(params.projectId);
 
+    // 유저 인증 확인
     if (!accessToken) return new HttpResponse(null, { status: 401 });
 
-    // 프로젝트에 속하는 모든 유저 검색
-    const projectUserList = PROJECT_USER_DUMMY.filter((row) => row.projectId === Number(projectId));
-    const userHash = getUserHash();
+    // 유저 ID 정보 취득
+    const userId = convertTokenToUserId(accessToken);
+    if (!userId) return new HttpResponse(null, { status: 401 });
 
-    const userList = projectUserList.map((relation) => {
-      const { userId, nickname } = userHash[relation.userId];
-      return { userId, nickname };
-    });
+    // 유저의 프로젝트 접근 권한 확인
+    const projectUser = findProjectUser(projectId, userId);
+    if (!projectUser) return new HttpResponse(null, { status: 403 });
+
+    // 프로젝트에 속하는 모든 유저 검색
+    const projectUsers = findAllProjectUser(projectId);
+
+    // 프로젝트 유저 정보 취득
+    const searchUsers: SearchUser[] = [];
+    for (let i = 0; i < projectUsers.length; i++) {
+      const user = findUser(projectUsers[i].userId);
+      if (!user) return new HttpResponse(null, { status: 404 });
+      searchUsers.push({ userId: user.userId, nickname: user.nickname });
+    }
 
     // 접두사(nickname)과 일치하는 유저 정보 최대 5명 추출
-    const prefixRegex = new RegExp(`^${nickname}`);
-    const matchedUserList = userList.filter((user) => prefixRegex.test(user.nickname)).slice(0, 5);
+    const matchedSearchUsers = searchUsers.filter((user) => user.nickname.startsWith(nickname)).slice(0, 5);
 
-    return HttpResponse.json(matchedUserList);
+    return HttpResponse.json(matchedSearchUsers);
   }),
 
   // 프로젝트 목록 조회 API
   http.get(`${BASE_URL}/team/:teamId/project`, ({ request, params }) => {
     const accessToken = request.headers.get('Authorization');
-    const { teamId } = params;
+    const teamId = Number(params.teamId);
 
+    // 유저 인증 확인
     if (!accessToken) return new HttpResponse(null, { status: 401 });
 
-    const projectList = PROJECT_DUMMY.filter((project) => project.teamId === Number(teamId));
+    // 유저 ID 정보 취득
+    const userId = convertTokenToUserId(accessToken);
+    if (!userId) return new HttpResponse(null, { status: 401 });
 
-    return HttpResponse.json(projectList);
+    // 유저의 팀 접근 권한 확인
+    const teamUser = findTeamUser(teamId, userId);
+    if (!teamUser) return new HttpResponse(null, { status: 403 });
+
+    // 특정 팀의 모든 프로젝트 조회
+    const projects = findAllProject(teamId);
+    return HttpResponse.json(projects);
   }),
 
   // 프로젝트 팀원 목록 조회 API
   http.get(`${BASE_URL}/project/:projectId/user`, ({ request, params }) => {
     const accessToken = request.headers.get('Authorization');
-    const { projectId } = params;
+    const projectId = Number(params.projectId);
 
+    // 유저 인증 확인
     if (!accessToken) return new HttpResponse(null, { status: 401 });
 
-    const projectUserList = PROJECT_USER_DUMMY.filter((projectUser) => projectUser.projectId === Number(projectId));
+    // 유저 ID 정보 취득
+    const userId = convertTokenToUserId(accessToken);
+    if (!userId) return new HttpResponse(null, { status: 401 });
 
-    const userHash = getUserHash();
-    const roleHash = getRoleHash();
-    const userRoleList = projectUserList.map((projectUser) => {
-      const { userId, nickname } = userHash[projectUser.userId];
-      const { roleName } = roleHash[projectUser.roleId];
-      return { userId, nickname, roleName };
-    });
+    // 프로젝트 정보 취득
+    const project = findProject(projectId);
+    if (!project) return new HttpResponse(null, { status: 404 });
 
-    return HttpResponse.json(userRoleList);
+    // 유저의 팀 접근 권한 확인
+    const teamUser = findTeamUser(project.teamId, userId);
+    if (!teamUser) return new HttpResponse(null, { status: 403 });
+
+    // 프로젝트의 모든 유저 조회
+    const projectUsers = findAllProjectUser(projectId);
+
+    // 프로젝트에 포함된 유저 정보 취득
+    const userRoles: UserWithRole[] = [];
+    for (let i = 0; i < projectUsers.length; i++) {
+      const { userId, roleId } = projectUsers[i];
+
+      const user = findUser(userId);
+      const role = findRole(roleId);
+      if (!user || !role) return new HttpResponse(null, { status: 404 });
+
+      userRoles.push({ userId, nickname: user.nickname, roleName: role.roleName });
+    }
+
+    return HttpResponse.json(userRoles);
   }),
 
   // 프로젝트 삭제 API
   http.delete(`${BASE_URL}/project/:projectId`, ({ request, params }) => {
     const accessToken = request.headers.get('Authorization');
-    const { projectId } = params;
+    const projectId = Number(params.projectId);
 
+    // 유저 인증 확인
     if (!accessToken) return new HttpResponse(null, { status: 401 });
 
-    const projectIdToDelete = Number(projectId);
+    // 유저 ID 정보 취득
+    const userId = convertTokenToUserId(accessToken);
+    if (!userId) return new HttpResponse(null, { status: 401 });
 
-    const statusIdsToDelete = STATUS_DUMMY.filter((status) => status.projectId === projectIdToDelete).map(
-      (status) => status.statusId,
-    );
+    // 유저의 프로젝트 접근 권한 확인
+    const projectUser = findProjectUser(projectId, userId);
+    if (!projectUser) return new HttpResponse(null, { status: 403 });
 
-    const filteredProjects = PROJECT_DUMMY.filter((project) => project.projectId !== projectIdToDelete);
-    if (PROJECT_DUMMY.length !== filteredProjects.length) {
-      PROJECT_DUMMY.length = 0;
-      PROJECT_DUMMY.push(...filteredProjects);
-    }
+    // 프로젝트의 모든 프로젝트 상태 ID 정보 취득
+    const statusIds = findAllProjectStatus(projectId).map((status) => status.statusId);
 
-    const filteredProjectUsers = PROJECT_USER_DUMMY.filter(
-      (projectUser) => projectUser.projectId !== projectIdToDelete,
-    );
-    if (PROJECT_USER_DUMMY.length !== filteredProjectUsers.length) {
-      PROJECT_USER_DUMMY.length = 0;
-      PROJECT_USER_DUMMY.push(...filteredProjectUsers);
-    }
-
-    const filteredStatuses = STATUS_DUMMY.filter((status) => status.projectId !== projectIdToDelete);
-    if (STATUS_DUMMY.length !== filteredStatuses.length) {
-      STATUS_DUMMY.length = 0;
-      STATUS_DUMMY.push(...filteredStatuses);
-    }
-
-    const filteredTasks = TASK_DUMMY.filter((task) => !statusIdsToDelete.includes(task.statusId));
-    if (TASK_DUMMY.length !== filteredTasks.length) {
-      TASK_DUMMY.length = 0;
-      TASK_DUMMY.push(...filteredTasks);
-    }
-
-    const filteredTaskUsers = TASK_USER_DUMMY.filter((taskUser) => {
-      const taskExists = TASK_DUMMY.some((task) => task.taskId === taskUser.taskId);
-      return taskExists;
+    // 프로젝트의 모든 일정 ID 정보 취득
+    const taskIds: number[] = [];
+    statusIds.forEach((statusId) => {
+      const tasks = findAllTask(statusId);
+      tasks.forEach((task) => taskIds.push(task.taskId));
     });
-    if (TASK_USER_DUMMY.length !== filteredTaskUsers.length) {
-      TASK_USER_DUMMY.length = 0;
-      TASK_USER_DUMMY.push(...filteredTaskUsers);
-    }
 
-    const filteredTaskFiles = TASK_FILE_DUMMY.filter((taskFile) => {
-      const taskExists = TASK_DUMMY.some((task) => task.taskId === taskFile.taskId);
-      return taskExists;
-    });
-    if (TASK_FILE_DUMMY.length !== filteredTaskFiles.length) {
-      TASK_FILE_DUMMY.length = 0;
-      TASK_FILE_DUMMY.push(...filteredTaskFiles);
+    // 프로젝트 삭제(순서 중요)
+    try {
+      taskIds.forEach((taskId) => {
+        deleteAllTaskFileInMemory(taskId);
+        deleteAllTaskFile(taskId);
+        deleteAllTaskUser(taskId);
+      });
+      deleteAllTask(projectId);
+      deleteAllProjectStatus(projectId);
+      deleteProject(projectId);
+      deleteAllProjectUser(projectId);
+    } catch (error) {
+      console.error((error as Error).message);
+      return new HttpResponse(null, { status: 500 });
     }
 
     return new HttpResponse(null, { status: 204 });
