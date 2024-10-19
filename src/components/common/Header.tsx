f7ceea6c155174ba89ddba3907b5fac20e2fe1e6
yoonyesol <maniac970430@naver.com>
2024-10-19 14:13:38 +0900

    Chore: #222 로그인 화면 이동 시 replace 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
         <button
           type="button"
           className="ml-10 h-20 rounded-md bg-white px-4 tracking-tight hover:brightness-90"
-          onClick={isAuthenticated ? handleLogout : () => navigate('/signin', { replace: true })}
+          onClick={isAuthenticated ? handleLogout : () => navigate('/signin')}
         >
           {isAuthenticated ? 'Logout' : 'Login'}
         </button>
