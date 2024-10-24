4463a4b97719e83822a0951272ff313a6a190653
seungchanwoo <tmdcksqkd123@naver.com>
2024-10-24 20:33:29 +0900

    UI: #241 프로젝트 수정 UI 구현

--- src/components/modal/project/UpdateModalProject.tsx
+++ src/components/modal/project/UpdateModalProject.tsx
@@ -1,10 +1,26 @@
 import ModalPortal from '@components/modal/ModalPortal';
 import ModalLayout from '@layouts/ModalLayout';
 import ModalButton from '@components/modal/ModalButton';
-import ModalProjectForm from '@components/modal/project/ModalProjectForm';
 
-import type { SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import PeriodDateInput from '@components/common/PeriodDateInput';
+import DescriptionTextarea from '@components/common/DescriptionTextarea';
+import DuplicationCheckInput from '@components/common/DuplicationCheckInput';
+import SearchUserInput from '@components/common/SearchUserInput';
+import UserRoleSelectBox from '@components/common/UserRoleSelectBox';
+import { useEffect, useMemo, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { PROJECT_VALIDATION_RULES } from '@constants/formValidationRules';
+import { findUserByTeam } from '@services/teamService';
+import useAxios from '@hooks/useAxios';
+import { PROJECT_DEFAULT_ROLE, PROJECT_ROLES } from '@constants/role';
+import Spinner from '@components/common/Spinner';
+import type { User } from '@/types/UserType';
+import type { Team } from '@/types/TeamType';
+import type { TeamSearchCallback } from '@/types/SearchCallbackType';
 import type { Project, ProjectForm } from '@/types/ProjectType';
+
+import { useReadProjectUserRoleList, useReadProjectDetail } from '@/hooks/query/useProjectQuery';
 
 type UpdateModalProjectProps = {
   projectId: Project['projectId'];
@@ -13,28 +29,120 @@ type UpdateModalProjectProps = {
 
 export default function UpdateModalProject({ projectId, onClose: handleClose }: UpdateModalProjectProps) {
   const updateProjectFormId = 'updateProjectForm';
+  const { teamId } = useParams();
+  const [keyword, setKeyword] = useState('');
 
-  const handleSubmit: SubmitHandler<ProjectForm> = async (data) => {
-    console.log('프로젝트 생성 폼 제출');
-    console.log(data);
+  const { projectInfo, isLoading: isProjectLoading } = useReadProjectDetail(Number(teamId), projectId);
+  const { projectUserRoleList: coworkers, isProjectUserRoleLoading: isProjectCoworkersLoading } =
+    useReadProjectUserRoleList(projectId);
+
+  const { loading, data: userList = [], clearData, fetchData } = useAxios(findUserByTeam);
+
+  const searchCallbackInfo: TeamSearchCallback = useMemo(
+    () => ({
+      type: 'TEAM',
+      searchCallback: (teamId: Team['teamId'], nickname: User['nickname']) => {
+        return fetchData(Number(teamId), nickname);
+      },
+    }),
+    [fetchData],
+  );
+
+  const methods = useForm<ProjectForm>({ mode: 'onChange' });
+  const {
+    register,
+    watch,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  useEffect(() => {
+    if (projectInfo) {
+      reset({
+        projectName: projectInfo.projectName,
+        content: projectInfo.content,
+        startDate: projectInfo.startDate.toISOString().split('T')[0],
+        endDate: projectInfo.endDate ? projectInfo.endDate.toISOString().split('T')[0] : null,
+      });
+    }
+  }, [projectInfo, reset]);
+
+  const handleKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value.trim());
+  };
+
+  const handleFormSubmit: SubmitHandler<ProjectForm> = async (data) => {
     handleClose();
   };
 
-  // ToDo: 프로젝트 수정 API 작업시 작성할 것
-  const handleUpdateClick = () => {};
+  if (isProjectLoading) {
+    return <Spinner />;
+  }
 
   return (
     <ModalPortal>
       <ModalLayout onClose={handleClose}>
-        <ModalProjectForm formId={updateProjectFormId} projectId={projectId} onSubmit={handleSubmit} />
-        <ModalButton
-          formId={updateProjectFormId}
-          color="text-white"
-          backgroundColor="bg-main"
-          onClick={handleUpdateClick}
-        >
+        <FormProvider {...methods}>
+          <form id="updateTeamForm" onSubmit={handleSubmit(handleFormSubmit)}>
+            <DuplicationCheckInput
+              id="projectName"
+              label="프로젝트 명"
+              value={watch('projectName')}
+              placeholder="프로젝트명을 입력해주세요."
+              errors={errors.projectName?.message}
+              register={register('projectName', PROJECT_VALIDATION_RULES.PROJECT_NAME)}
+            />
+
+            <DescriptionTextarea
+              id="content"
+              label="프로젝트 설명"
+              fieldName="content"
+              placeholder="프로젝트 내용을 입력해주세요."
+              validationRole={PROJECT_VALIDATION_RULES.PROJECT_DESCRIPTION}
+              errors={errors.content?.message}
+            />
+
+            <PeriodDateInput
+              startDateLabel="시작일"
+              endDateLabel="종료일"
+              startDateId="startDate"
+              endDateId="endDate"
+              startDateFieldName="startDate"
+              endDateFieldName="endDate"
+            />
+          </form>
+        </FormProvider>
+
+        <ModalButton formId={updateProjectFormId} color="text-white" backgroundColor="bg-main">
           수정
         </ModalButton>
+        <div className="my-16">
+          <SearchUserInput
+            id="search"
+            label="팀원"
+            keyword={keyword}
+            loading={loading}
+            userList={userList}
+            searchId={Number(teamId)}
+            searchCallbackInfo={searchCallbackInfo}
+            onKeywordChange={handleKeywordChange}
+            onUserClick={() => {}} // 팀원 추가 클릭 핸들러
+          />
+          <div className="flex flex-wrap">
+            {coworkers.map(({ userId, nickname }) => (
+              <UserRoleSelectBox
+                key={userId}
+                userId={userId}
+                nickname={nickname}
+                roles={PROJECT_ROLES}
+                defaultValue={PROJECT_DEFAULT_ROLE}
+                onRoleChange={() => {}} // 역할 변경 핸들러
+                onRemoveUser={() => {}} // 팀원 삭제 핸들러
+              />
+            ))}
+          </div>
+        </div>
       </ModalLayout>
     </ModalPortal>
   );
