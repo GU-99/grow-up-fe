da23ef3ffa21c5785e63c9709f26945f1190be2f
seungchanwoo <tmdcksqkd123@naver.com>
2024-11-04 20:16:04 +0900

    Formatting: #257 팀 & 프로젝트 권한 설명 변경

--- src/constants/role.ts
+++ src/constants/role.ts
@@ -11,26 +11,35 @@ export const TEAM_ROLE_INFO: RoleInfo[] = [
   {
     roleName: 'HEAD',
     label: 'HEAD',
-    description: '팀의 모든 기능 가능',
+    description: '팀의 모든 기능을 사용할 수 있습니다.\n권한 관리, 팀원 초대 및 추방\n팀과 프로젝트 설정 및 관리 등',
   },
   {
     roleName: 'LEADER',
     label: 'LEADER',
-    description: '팀 및 프로젝트 읽기 가능\n생성/수정/삭제의 경우 본인이 작성한것만 가능',
+    description: '팀과 프로젝트의 내용을 볼 수 있으며,\n본인이 작성한 항목은 추가, 수정, 삭제가 가능합니다.',
   },
-  { roleName: 'MATE', label: 'Mate', description: '팀 조작 중 프로젝트 읽기 기능만 가능' },
+  {
+    roleName: 'MATE',
+    label: 'Mate',
+    description: '팀에서 프로젝트의 내용을 읽을 수 있습니다.\n추가, 수정, 삭제는 불가능합니다.',
+  },
 ];
 
 export const PROJECT_ROLE_INFO: RoleInfo[] = [
-  { roleName: 'ADMIN', label: 'ADMIN', description: '프로젝트 모든 권한 가능' },
+  {
+    roleName: 'ADMIN',
+    label: 'ADMIN',
+    description:
+      '프로젝트의 모든 기능을 사용할 수 있습니다.\n권한 관리, 프로젝트 멤버 초대 및 제거\n상태 및 일정 관리 등',
+  },
   {
     roleName: 'LEADER',
     label: 'LEADER',
-    description: `상태 및 일정 읽기 가능 \n생성/수정/삭제 가능`,
+    description: '프로젝트의 상태 및 일정을 보고\n새로 추가하거나 수정, 삭제할 수 있습니다.',
   },
   {
     roleName: 'ASSIGNEE',
     label: 'ASSIGNEE',
-    description: '일정 읽기 가능\n생성/수정/삭제 본인이 작성한 것만 가능 ',
+    description: '프로젝트 일정을 보고\n본인이 작성한 일정만 추가, 수정 및 삭제가 가능합니다.',
   },
 ];
