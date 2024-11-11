4876100be2d393e887fedc1d28efcf456833e040
seungchanwoo <tmdcksqkd123@naver.com>
2024-11-11 20:55:44 +0900

    Feat: 프로젝트 생성 및 수정 권한 설정 추가

--- src/pages/team/TeamPage.tsx
+++ src/pages/team/TeamPage.tsx
@@ -5,34 +5,52 @@ import { useState } from 'react';
 import CreateModalProject from '@components/modal/project/CreateModalProject';
 import useModal from '@hooks/useModal';
 import UpdateModalProject from '@components/modal/project/UpdateModalProject';
-import { useReadProjects, useDeleteProject } from '@hooks/query/useProjectQuery';
+import { useReadProjects, useDeleteProject, useReadProjectCoworkers } from '@hooks/query/useProjectQuery';
 import Spinner from '@components/common/Spinner';
-import { useReadTeams } from '@hooks/query/useTeamQuery';
+import { useReadTeamCoworkers, useReadTeams } from '@hooks/query/useTeamQuery';
 
 import useToast from '@hooks/useToast';
+import { useStore } from '@stores/useStore';
 import type { Project } from '@/types/ProjectType';
 
 export default function TeamPage() {
   const { showModal: showProjectModal, openModal: openProjectModal, closeModal: closeProjectModal } = useModal();
   const { showModal: showUpdateModal, openModal: openUpdateModal, closeModal: closeUpdateModal } = useModal();
   const { teamId } = useParams();
+  const { userInfo } = useStore();
+  const { userId } = userInfo;
+  const { coworkers: teamCoworkers } = useReadTeamCoworkers(Number(teamId));
+  const { projectCoworkers } = useReadProjectCoworkers(Number(teamId));
+
   const { projectList: teamProjects, isProjectLoading } = useReadProjects(Number(teamId));
   const { joinedTeamList, isLoading: isTeamLoading } = useReadTeams();
   const [selectedProjectId, setSelectedProjectId] = useState<Project['projectId'] | null>(null);
-  const { toastWarn } = useToast();
+  const { toastWarn, toastError } = useToast();
 
   const { mutate: deleteProjectMutate } = useDeleteProject(Number(teamId));
 
+  const userTeamRole = teamCoworkers.find((coworker) => coworker.userId === userId)?.roleName || null;
+  const userProjectRole = projectCoworkers.find((coworker) => coworker.userId === userId)?.roleName;
   const team = joinedTeamList.find((team) => team.teamId.toString() === teamId);
   const teamName = team ? team.teamName : '';
 
   const handleOpenUpdateModal = (projectId: Project['projectId']) => {
+    if (userProjectRole !== 'ADMIN') {
+      return toastError('프로젝트 수정 권한이 없습니다.');
+    }
+
     setSelectedProjectId(projectId);
     openUpdateModal();
   };
 
   const handleCreateProjectClick = () => {
     if (!teamId) return toastWarn('팀을 선택한 후 프로젝트 생성을 진행해주세요.');
+
+    if (userTeamRole !== 'HEAD' && userTeamRole !== 'LEADER') {
+      toastError('프로젝트 생성 권한이 없습니다.');
+      return;
+    }
+
     openProjectModal();
   };
 
