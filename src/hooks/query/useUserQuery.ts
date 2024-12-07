d4c5670cfac918fd323635b29ad04c38869f9d3f
yoonyesol <maniac970430@naver.com>
2024-12-07 23:14:14 +0900

    Fix: #279 이미지 삭제 시 파일 변수명 변경

--- src/hooks/query/useUserQuery.ts
+++ src/hooks/query/useUserQuery.ts
@@ -59,7 +59,7 @@ export function useDeleteProfileImage() {
     onError: () => toastError('이미지 삭제에 실패했습니다. 다시 시도해 주세요.'),
     onSuccess: () => {
       toastSuccess('이미지가 삭제되었습니다.');
-      editUserInfo({ profileImageName: null });
+      editUserInfo({ fileName: null });
       queryClient.invalidateQueries({ queryKey: userProfileImageQueryKey });
     },
   });
